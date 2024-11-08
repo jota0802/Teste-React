62fe7d839ad7b16701049df071a1eb65a650a5ea
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-08 14:54:35 -0300

    estrutura basica feita

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from './components/Nav/Nav'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+function App() {  return (
+    <>
+      <Nav/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
